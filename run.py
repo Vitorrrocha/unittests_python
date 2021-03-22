c87ced31ca90cc6f279b5d404d76d0dd05a7ddf4
vitorrrocha <vitor@gntech.med.br>
2021-03-22 15:06:40 -0300

    Feat: unit tests add tests

---
+++ run.py
@@ -0,0 +1,10 @@
+from src.calculadora import Calculator
+from src.operations.add import AddOperation
+from src.operations.sub import SubOperation
+
+calc = Calculator(AddOperation(), SubOperation())
+op1 = calc.add(2, 5, True)
+op2 = calc.sub(5, 3, False)
+
+print(op1)
+print(op2)
